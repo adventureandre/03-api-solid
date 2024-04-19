48a7e5fd002b39e44981aee43f008d6aeb075439
Andre Luiz <adventureandre@hotmail.com>
2024-04-19 18:11:00 -0300

    Configurando Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app =  fastify()
+
+
+const prisma = new PrismaClient()
+
+prisma.user.create({data:{name:"123, email:"andre@andre"}})
