7051a3eca22eb90305d72b92859f7bcb1a45b447
Andre Luiz <adventureandre@hotmail.com>
2024-04-22 12:31:35 -0300

    inversao de dependencias

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,33 +1,29 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import {hash} from 'bcryptjs'
-import { prisma } from "@/lib/prisma"
-import { z } from "zod"
-
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { InMemoryUsersRepository } from '@/repositories/in-memory-user-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-    const registerBobySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
     })
+  } catch (err) {
+    return reply.status(409).send()
+  }
 
-    const { name, email, password } = registerBobySchema.parse(request.body)
-
-    const password_hash = await hash(password,6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email,
-        }
-    })
-
-    if (userWithSameEmail){
-        return reply.status(409).send()
-    }
-
-    await prisma.user.create({
-        data: { name, email, password_hash }
-    })
-
-    return reply.status(201).send()
+  return reply.status(201).send()
 }
