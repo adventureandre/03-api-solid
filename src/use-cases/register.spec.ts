95b437da8ef7aa85c535657694269fdaf7fe4337
Andre Luiz <adventureandre@hotmail.com>
2024-04-23 21:07:23 -0300

    Primeiro teste unitário

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,39 @@
-import { expect, test } from "vitest"
+import { expect, test, describe, it } from "vitest"
+import { RegisterUseCase } from "./register"
+import { compare } from "bcryptjs"
 
 
-test('check if it works', () => {
-    expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email) {
+                return null;
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date(),
+
+                }
+            },
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: 'andre',
+            email: 'andre@andre.com',
+            password: '123456'
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
 })
