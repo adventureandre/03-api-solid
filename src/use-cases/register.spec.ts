7f1fee9e1c49fcdbce7bf1b46cab03078fcfde9f
Andre luiz <adventureandre@hotmail.com>
2024-10-17 21:44:40 -0300

    feat: teste and create controller

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -48,7 +48,7 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    await expect(() =>
+    expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,
