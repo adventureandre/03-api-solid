c3533051e6e338b088800aa988f90223e5cc6fa2
André Luiz <adventureandre@hotmail.com>
2025-04-19 19:54:42 -0300

    feat: adicionar função para gerar URL do banco de dados no ambiente de teste Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,29 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest/environments'
+
+// postgresql://docker:docker@localhost:5432/api-solid?schema=public
+
+function generateDatabaseUrl(schema: string) {
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+
+    console.log(generateDatabaseUrl(schema))
 
     return {
       teardown() {
