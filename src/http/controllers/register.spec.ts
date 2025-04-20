188851a2687f75a111542623d4497861a84f4f06
André Luiz <adventureandre@hotmail.com>
2025-04-19 23:34:45 -0300

    chore: update package.json dependencies and add supertest for testing
    feat: implement end-to-end tests for user registration

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,25 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-   
+describe('Register (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register', async () => {
+        const reponse = await request(app.server)
+            .post('/users')
+            .send({
+                name: "John Doe",
+                email: 'johndoe@exemple.com',
+                password: '123456'
+            })
+
+        expect(reponse.statusCode).toEqual(201)
+    })
 })
