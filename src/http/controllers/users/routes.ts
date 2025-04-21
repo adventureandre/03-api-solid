100efc23ed0f82514022c84d75261408d71c5aa3
André Luiz <adventureandre@hotmail.com>
2025-04-21 12:37:20 -0300

    feat: adicionar suporte a refresh token com cookies e implementar testes e2e

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { authenticate } from '@/http/controllers/users/authenticate'
 import { register } from './register'
 import { profile } from './profile'
 import { verifyJwt } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh',refresh)
 
   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
